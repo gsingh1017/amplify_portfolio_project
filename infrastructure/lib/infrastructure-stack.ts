599299230580f86a5cae4ca7b87a9c4a78f067e6
gsingh1017 <182170594+gsingh1017@users.noreply.github.com>
2025-06-24 16:54:58 -0700

    Cleanup code

--- infrastructure/lib/infrastructure-stack.ts
+++ infrastructure/lib/infrastructure-stack.ts
@@ -1,10 +1,10 @@
 import * as cdk from 'aws-cdk-lib';
 import * as amplify from '@aws-cdk/aws-amplify-alpha';
 import * as codebuild from 'aws-cdk-lib/aws-codebuild';
-import * as route53 from 'aws-cdk-lib/aws-route53';
 
 
 export class InfrastructureStack extends cdk.Stack {
+
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
@@ -26,14 +26,14 @@ export class InfrastructureStack extends cdk.Stack {
             preBuild: {
               commands: [
                 'echo "Starting pre-build phase..."',
-                'cd portfolio-website',
-                'npm ci --no-audit --no-fund',
+                'cd portfolio-website', // Navigate to the Next.js project directory
+                'npm ci --no-audit --no-fund', // Install clean dependencies
               ],
             },
             build: {
               commands: [
                 'echo "Starting NextJS build phase..."',
-                'npm run build',
+                'npm run build', // Build the Next.js application
                 'echo "Build completed successfully!"',
               ],
             },
